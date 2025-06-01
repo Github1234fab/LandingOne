04c732d34d366dd1a59322f99b8b7374a3737ec5
Github1234fab <fabien.marceau@gmail.com>
2025-06-01 16:29:46 +0200

    ok

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,3 @@
 
 
-	<!-- Form caché pour que Netlify le voie -->
-	<form name="contact-form-solairConfort" netlify hidden>
-		<input type="text" name="name" />
-		<input type="email" name="email" />
-		<input type="tel" name="phone" />
-		<textarea name="message"></textarea>
-	</form>
 <slot />
