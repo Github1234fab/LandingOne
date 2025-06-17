a535af64625fd2823b360af40edf91de39cb65d5
Github1234fab <fabien.marceau@gmail.com>
2025-06-17 11:39:52 +0200

    ok

--- src/routes/merci/+page.svelte
+++ src/routes/merci/+page.svelte
@@ -1,29 +1,29 @@
-
 <script>
-     import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
-onMount(() => {
-  gtag('event', 'conversion', {
-    'send_to': 'AW-17160903163/tXCACLnDptkaEPuz-vY_',  // Remplace YYY par ton label de conversion exact
-    'value': 1.0,
-    'currency': 'EUR'
-  });
-});
-  </script>
-
+	onMount(() => {
+		gtag('event', 'conversion', {
+			send_to: 'AW-17160903163/tXCACLnDptkaEPuz-vY_', // Remplace YYY par ton label de conversion exact
+			value: 1.0,
+			currency: 'EUR'
+		});
+	});
+</script>
 
 <main id="merci" class="merci-page">
-    <h1>Merci pour votre demande !</h1>
-    <p>Nous vous recontacterons dans les plus brefs délais.</p>
-    <p>Vous allez être redirigé vers la page d'accueil dans quelques secondes…</p>
+	<h1>Merci pour votre demande !</h1>
+	<br />
+	<p>Nous vous recontacterons dans les plus brefs délais.</p>
+	<br />
+	<a href="/">Revenir sur l'offre</a>
+</main>
 
-  </main>
-  
-  <style>
-    .merci-page {
-      max-width: 600px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-  </style>
+<style>
+	.merci-page {
+		max-width: 600px;
+		margin: 0 auto;
+		padding: 2rem;
+		text-align: center;
+		font-family: Poppins, sans-serif;
+	}
+</style>
